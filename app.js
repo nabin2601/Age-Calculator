433da3fd22a6c84184b5b6d0c44f14c41de5f5cb
nabin2601 <nabinb943@gmail.com>
2024-06-16 21:33:36 +1000

    app.js changes

--- app.js
+++ app.js
@@ -5,27 +5,39 @@ const result = document.getElementById("result");
 
 ageInput.max = new Date().toISOString().split("T")[0]
 
-btn.addEventListener('click', () => {
-    if (!ageInput.value) {
-        result.textContent = "Please select your date of birth."
-        return;
-    };
+const displayResult = () => {
+  if (!ageInput.value) {
+    result.textContent = 'Please select your date of birth.';
+    return;
+  }
 
-    const dobDate = new Date(ageInput.value);
-    const currentDate = new Date();
+  const dobDate = new Date(ageInput.value);
+  const currentDate = new Date();
 
-    let years = currentDate.getFullYear() - dobDate.getFullYear();
-    let month = currentDate.getMonth() - dobDate.getMonth();
-    let days = currentDate.getDate() - dobDate.getDate();
+  let years = currentDate.getFullYear() - dobDate.getFullYear();
+  let month = currentDate.getMonth() - dobDate.getMonth();
+  let days = currentDate.getDate() - dobDate.getDate();
 
-    if (days<0) {
-        month--;
-        const prevMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0);
-        days += prevMonth.getDate();
-    }
-    if (month < 0) {
-        years--;
-        month += 12;
-    }
-    result.textContent = `Your age is ${years} years, ${month} Months and ${days} days.`;s
-})
+  if (days < 0) {
+    month--;
+    const prevMonth = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      0
+    );
+    days += prevMonth.getDate();
+  }
+  if (month < 0) {
+    years--;
+    month += 12;
+  }
+  result.textContent = `Your age is ${years} years, ${month} Months and ${days} days.`;
+};
+
+btn.addEventListener('click', () => displayResult());
+ageInput.addEventListener('keydown', (e) => {
+  e.preventDefault();
+  if ('Enter') {
+    displayResult();
+  }
+});
